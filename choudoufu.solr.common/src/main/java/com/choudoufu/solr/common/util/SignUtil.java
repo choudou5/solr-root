47d7bc261673b51e08a050222051d5cfaa9ef8c1
choudou5 <xuhaowende@sina.cn>
2017-02-15 23:58:58 +0800

    更新签名  采用XXTea算法

--- choudoufu.solr.common/src/main/java/com/choudoufu/solr/common/util/SignUtil.java
+++ choudoufu.solr.common/src/main/java/com/choudoufu/solr/common/util/SignUtil.java
@@ -1,7 +1,13 @@
 package com.choudoufu.solr.common.util;
 
+import java.math.BigInteger;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.commons.lang.RandomStringUtils;
-import org.apache.commons.lang.StringUtils;
 
 /**
  * 签名工具
@@ -9,47 +15,29 @@ import org.apache.commons.lang.StringUtils;
  * @date 2017年2月13日
  */
 public class SignUtil {
-
+	
+	/** 签名key */
+	private static final String SIG_KEY = "solrroot";
+	
 	public static void main(String[] args) {
 		long begin = System.currentTimeMillis();
-		for (int i = 0; i < 800000; i++) {
-			decryptCode(getEncryptCode());
+		for (int i = 0; i < 100; i++) {
+			String encrypt = getEncryptCode();
+			System.out.println("encrypt="+encrypt+", decryptCode="+decryptCode(encrypt));
 		}
 		long end = System.currentTimeMillis();
 		System.out.println("time:"+(end-begin));
 	}
 	
-	private static final char[] SPECIAL_SYMBOLS = new char[]{'a','b','c','d','e','g','f'};
-	
 	/**
-	 * 获得 加密 code
-	 * @param code
+	 * 获得 加密 code (XXTea算法)
 	 * @return
 	 */
 	public static String getEncryptCode(){
-		int rdLen = 10;
-		String beginCode = RandomStringUtils.randomAlphanumeric(rdLen);
-		
-		String key = getCodeKey(beginCode);
-		String endCode = RandomStringUtils.randomAlphabetic(rdLen);
-		int endCodeLen = endCode.length();
-		int keyLen = key.length();
-		StringBuilder codeBuilder = new StringBuilder(endCodeLen+keyLen);
-		if(isReverse(key))
-			key = StringUtils.reverse(key);
-		
-		for (int i = 0; i < endCodeLen; i++) {
-			char c = endCode.charAt(i);
-			codeBuilder.append(c);
-			int sm = c%SPECIAL_SYMBOLS.length;
-			if(sm == 0)
-				codeBuilder.append(SPECIAL_SYMBOLS[sm]);
-			if(keyLen > i)
-				codeBuilder.append(key.charAt(i));
-		}
-		return beginCode+","+codeBuilder.toString();
-		
+		String code = RandomStringUtils.randomAlphanumeric(10);
+		return code+","+XXTea.encrypt(code+XXTea.getFactors(SIG_KEY), SIG_KEY);
 	}
+	
 	
 	/**
 	 * 解密 encryptCode
@@ -59,42 +47,194 @@ public class SignUtil {
 	public static boolean decryptCode(String encryptCode){
 		if(encryptCode == null)
 			return false;
+		
 		String[] codes = encryptCode.split(",");
 		if(codes.length != 2)
 			return false;
 		
-		String key = getCodeKey(codes[0]);
-		String sig = codes[1];
-		
-		StringBuilder numStr = new StringBuilder(key.length());
-		for (int i = 0; i < sig.length(); i++) {
-			char c = sig.charAt(i);
-			if(c >= 48 && c <= 57)//0-9
-				numStr.append(c);
-		}
-		String decrKey = numStr.toString();
-		if(isReverse(key)){
-			decrKey = numStr.reverse().toString();
-		}
-		return key.equals(decrKey);
+		String code = codes[0];
+		String encrypt = codes[1];
+		String decrypt = XXTea.decrypt(encrypt, SIG_KEY);
+		return decrypt.equals(code+XXTea.getFactors(SIG_KEY));
 	}
 
-	private static boolean isReverse(String key){
-		return (Integer.parseInt(key)%2)==0?true:false;
-	}
-	
-	private static String getCodeKey(String code){
-		if(StringUtils.isBlank(code))
-			return "";
-		int len = code.length();
-		int sum = 0;
+	/**
+	 * 对称加密解密算法
+	 * @author xuhaowen
+	 * @date 2017年2月15日
+	 */
+	private static class XXTea {
+
+		private static long DELTA = 2654435769L;
+	    private static int MIN_LENGTH = 32;
+	    private static char SPECIAL_CHAR = '\0';
+	    
+	    //-----------begin----------扩展 XXTea 增加 加密因子--------------
+	    private static final char[] FACTORS = new char[]{'a','b','c','d','e','g','f'};
 		
-		for (int i = 0; i < len; i++) {
-			char c = code.charAt(i);
-			int sm = c%SPECIAL_SYMBOLS.length;
-			sum += (int)c+((i+1)*len*(int)SPECIAL_SYMBOLS[sm]);
+		/**
+		 * 获取 加密影响因子
+		 * @return
+		 */
+		public static String getFactors(String sigKey){
+			int sigLen = sigKey.length();
+			StringBuilder strBuilder = new StringBuilder(sigLen);
+			for (int i = 0; i < sigLen; i++) {
+				char c = sigKey.charAt(i);
+				int f = c%FACTORS.length;
+				strBuilder.append(FACTORS[f]);
+			}
+			return strBuilder.toString();
 		}
-		return sum+"";
+		//-----------end----------扩展 XXTea 增加 加密因子--------------
+	    
+		public static String encrypt(String data, String key) {
+	        return toHexString(teaEncrypt(
+	                toLongArray(padRight(data, MIN_LENGTH).getBytes(Charset.forName("UTF8"))),
+	                toLongArray(padRight(key, MIN_LENGTH).getBytes(Charset.forName("UTF8")))));
+	    }
+	 
+	    public static String decrypt(String data, String key) {
+	        if (data == null || data.length() < MIN_LENGTH) {
+	            return data;
+	        }
+	        byte[] code = toByteArray(teaDecrypt(
+	        		toLongArray(data),
+	                toLongArray(padRight(key, MIN_LENGTH).getBytes(Charset.forName("UTF8")))));
+	        return new String(code, Charset.forName("UTF8"));
+	    }
+	 
+	    private static long[] teaEncrypt(long[] data, long[] key) {
+	        int n = data.length;
+	        if (n < 1) {
+	            return data;
+	        }
+	 
+	        long z = data[data.length - 1], y = data[0], sum = 0, e, p, q;
+	        q = 6 + 52 / n;
+	        while (q-- > 0) {
+	            sum += DELTA;
+	            e = (sum >> 2) & 3;
+	            for (p = 0; p < n - 1; p++) {
+	                y = data[(int) (p + 1)];
+	                z = data[(int) p] += (z >> 5 ^ y << 2) + (y >> 3 ^ z << 4)
+	                        ^ (sum ^ y) + (key[(int) (p & 3 ^ e)] ^ z);
+	            }
+	            y = data[0];
+	            z = data[n - 1] += (z >> 5 ^ y << 2) + (y >> 3 ^ z << 4)
+	                    ^ (sum ^ y) + (key[(int) (p & 3 ^ e)] ^ z);
+	        }
+	 
+	        return data;
+	    }
+	 
+	    private static long[] teaDecrypt(long[] data, long[] key) {
+	        int n = data.length;
+	        if (n < 1) {
+	            return data;
+	        }
+	 
+	        long z = data[data.length - 1], y = data[0], sum = 0, e, p, q;
+	        q = 6 + 52 / n;
+	        sum = q * DELTA;
+	        while (sum != 0) {
+	            e = (sum >> 2) & 3;
+	            for (p = n - 1; p > 0; p--) {
+	                z = data[(int) (p - 1)];
+	                y = data[(int) p] -= (z >> 5 ^ y << 2) + (y >> 3 ^ z << 4)
+	                        ^ (sum ^ y) + (key[(int) (p & 3 ^ e)] ^ z);
+	            }
+	            z = data[n - 1];
+	            y = data[0] -= (z >> 5 ^ y << 2) + (y >> 3 ^ z << 4) ^ (sum ^ y)
+	                    + (key[(int) (p & 3 ^ e)] ^ z);
+	            sum -= DELTA;
+	        }
+	 
+	        return data;
+	    }
+	 
+	    private static long[] toLongArray(byte[] data) {
+	        int n = (data.length % 8 == 0 ? 0 : 1) + data.length / 8;
+	        long[] result = new long[n];
+	 
+	        for (int i = 0; i < n - 1; i++) {
+	            result[i] = bytes2long(data, i * 8);
+	        }
+	 
+	        byte[] buffer = new byte[8];
+	        for (int i = 0, j = (n - 1) * 8; j < data.length; i++, j++) {
+	            buffer[i] = data[j];
+	        }
+	        result[n - 1] = bytes2long(buffer, 0);
+	 
+	        return result;
+	    }
+	 
+	    private static byte[] toByteArray(long[] data) {
+	        List<Byte> result = new ArrayList<Byte>();
+	 
+	        for (int i = 0; i < data.length; i++) {
+	            byte[] bs = long2bytes(data[i]);
+	            for (int j = 0; j < 8; j++) {
+	                result.add(bs[j]);
+	            }
+	        }
+	 
+	        while (result.get(result.size() - 1) == SPECIAL_CHAR) {
+	            result.remove(result.size() - 1);
+	        }
+	 
+	        byte[] ret = new byte[result.size()];
+	        for (int i = 0; i < ret.length; i++) {
+	            ret[i] = result.get(i);
+	        }
+	        return ret;
+	    }
+	 
+	    public static byte[] long2bytes(long num) {
+	        ByteBuffer buffer = ByteBuffer.allocate(8).order(
+	                ByteOrder.LITTLE_ENDIAN);
+	        buffer.putLong(num);
+	        return buffer.array();
+	    }
+	 
+	    public static long bytes2long(byte[] b, int index) {
+	        ByteBuffer buffer = ByteBuffer.allocate(8).order(
+	                ByteOrder.LITTLE_ENDIAN);
+	        buffer.put(b, index, 8);
+	        return buffer.getLong(0);
+	    }
+	 
+	    private static String toHexString(long[] data) {
+	        StringBuilder sb = new StringBuilder();
+	        for (int i = 0; i < data.length; i++) {
+	            sb.append(padLeft(Long.toHexString(data[i]), 16));
+	        }
+	        return sb.toString();
+	    }
+	 
+	    private static long[] toLongArray(String data) {
+	        int len = data.length() / 16;
+	        long[] result = new long[len];
+	        for (int i = 0; i < len; i++) {
+	            result[i] = new BigInteger(data.substring(i * 16, i * 16 + 16), 16)
+	                    .longValue();
+	        }
+	        return result;
+	    }
+	 
+	    private static String padRight(String source, int length) {
+	        while (source.length() < length) {
+	            source += SPECIAL_CHAR;
+	        }
+	        return source;
+	    }
+	 
+	    private static String padLeft(String source, int length) {
+	        while (source.length() < length) {
+	            source = '0' + source;
+	        }
+	        return source;
+	    }
 	}
-	
 }
